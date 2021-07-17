78fd268a032a7424113a25002cb4b104c0a89998
Dima Fedin <fedinamid@gmail.com>
2021-07-17 18:24:40 +0300

    finish homework

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,62 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import { allPass } from "ramda";
+import Api from "../tools/api";
 
- const api = new Api();
- 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
- 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
- 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
- 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
- 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
- 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
- 
- export default processSequence;
+const api = new Api();
+
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  try {
+    writeLog(value);
+
+    const validator = allPass([
+      (value) => value.length < 10,
+      (value) => value.length > 2,
+      (value) => Number(value) > 0,
+      (value) => /[+-]?([0-9]*[.])?[0-9]+/.test(value),
+    ]);
+
+    if (!validator(value)) {
+      return handleError("ValidationError");
+    }
+
+    const normalizedValue = Math.round(Number(value));
+
+    api
+      .get("https://api.tech/numbers/base", {
+        from: 10,
+        to: 2,
+        number: normalizedValue,
+      })
+      .then(({ result: binaryNumber }) => {
+        writeLog(binaryNumber.length);
+        writeLog(binaryNumber.length * binaryNumber.length);
+        writeLog(binaryNumber.length % 3);
+
+        api
+          .get(`https://animals.tech/${binaryNumber.length % 3}`)({})
+          .then(({ result }) => {
+            handleSuccess(result);
+          });
+      });
+  } catch (err) {
+    console.log({ err });
+    handleError(err);
+  }
+};
+
+export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,6 +1,6 @@
 /**
  * @file Домашка по FP ч. 1
- * 
+ *
  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
  * Эти функции/их аналоги есть и в ramda и в lodash
  *
@@ -12,39 +12,121 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
+import { propEq, allPass, anyPass } from "ramda";
+
+const getFiguresByColor = (figures) => {
+  let figuresByColor = {
+    blue: 0,
+    red: 0,
+    green: 0,
+    orange: 0,
+    white: 0,
+  };
+
+  for (let figure in figures) {
+    figuresByColor[figures[figure]]++;
+  }
+
+  return figuresByColor;
+};
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+export const validateFieldN1 = ({ star, square, triangle, circle }) => {
+  if (triangle !== "white" || circle !== "white") {
+    return false;
+  }
 
-    return star === 'red' && square === 'green';
+  const validator = allPass([
+    propEq("star", "red"),
+    propEq("square", "green"),
+    propEq("triangle", "white"),
+    propEq("circle", "white"),
+  ]);
+
+  return validator({ star, square, triangle, circle });
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (figures) => {
+  const { green } = getFiguresByColor(figures);
+
+  return green >= 2;
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (figures) => {
+  const { blue, red } = getFiguresByColor(figures);
+
+  return blue === red;
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = (figures) => {
+  const validator = allPass([
+    propEq("circle", "blue"),
+    propEq("star", "red"),
+    propEq("square", "orange"),
+  ]);
+
+  return validator(figures);
+};
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = (figures) => {
+  const figuresByColor = getFiguresByColor(figures);
+
+  for (let color in figuresByColor) {
+    if (figuresByColor[color] >= 3 && color !== "white") {
+      return true;
+    }
+  }
+
+  return false;
+};
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (figures) => {
+  const { green, red } = getFiguresByColor(figures);
+  const validator = allPass([
+    propEq("triangle", "green"),
+    propEq("green", 2),
+    propEq("red", 1),
+  ]);
+
+  return validator({ triangle: figures.triangle, green, red });
+};
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (figures) => {
+  const validator = allPass([
+    propEq("star", "orange"),
+    propEq("triangle", "orange"),
+    propEq("square", "orange"),
+    propEq("circle", "orange"),
+  ]);
+
+  return validator(figures);
+};
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = (figures) => {
+  const validator = anyPass([propEq("star", "red"), propEq("star", "white")]);
+
+  return !validator(figures);
+};
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (figures) => {
+  const validator = allPass([
+    propEq("star", "green"),
+    propEq("triangle", "green"),
+    propEq("square", "green"),
+    propEq("circle", "green"),
+  ]);
+
+  return validator(figures);
+};
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = (figures) =>
+  figures.triangle === figures.square;
